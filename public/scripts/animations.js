81a2d095ec0967435e3bad8e3824eedba9cd039f
leor8 <leo442183205@gmail.com>
2018-06-29 23:57:10 +0000

    fixing bugs where only one card can be edited

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  if (!("geolocation" in navigator)) {
+
+  }
+
   // Smooth scrolling on anchor
   $(document).on('click', 'a[href^="#"]', function (event){
     event.preventDefault();
@@ -10,65 +14,61 @@ $(document).ready(function() {
   });
 
   // Sending alerts upon empty input
-  $('.alert').hide();
+  //$('.alert').hide();
 
-  $(".btn_submit").click(function() {
-<<<<<<< HEAD
-=======
-    //perform ajax post request
-    console.log($(".new_todo_input").val());
-    var textboxval =$(".new_todo_input").val();
-    const usercoordinate = navigator.geolocation.getCurrentPosition((position) => {
-      const lat  = position.coords.latitude;
-      const long = position.coords.longitude;
-      console.log(textboxval);
-      $.ajax({datatype: "json", 
-      url: '/todos', 
-      data: {text: textboxval,lat: lat , long: long}, 
-      type: 'POST',
-      success: function(responseData, textStatus, jqXHR) {
-          console.log(responseData);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          console.log(errorThrown);
-      }
-});
+  // $(".btn_submit").click(function() {
+  //   //perform ajax post request
+  //   console.log($(".new_todo_input").val());
+  //   var textboxval =$(".new_todo_input").val();
+  //   const usercoordinate = navigator.geolocation.getCurrentPosition((position) => {
+  //     const lat  = position.coords.latitude;
+  //     const long = position.coords.longitude;
+  //     $.ajax({datatype: "json",
+  //     url: '/todos',
+  //     data: {text: textboxval,lat: lat , long: long},
+  //     type: 'POST',
+  //     success: function(responseData, textStatus, jqXHR) {
+  //         console.log(responseData);
+  //     },
+  //     error: function(jqXHR, textStatus, errorThrown) {
+  //         console.log(errorThrown);
+  //     }
+  //   });
 
 
-    });
+  // });
 
->>>>>>> a9f7b3d7dc88a27939847e12806824636b158dba
-    $('.btn_submit').removeAttr("data-dismiss");
-    if(!$(".new_todo_input").val()){
-      $('.alert').show();
-    } else {
-      $(".alert").hide();
-      $(".new_todo_input").val("");
-      $('.btn_submit').attr("data-dismiss", "modal");
-    }
-  })
+  // $('.btn_submit').removeAttr("data-dismiss");
+  //   if(!$(".new_todo_input").val()){
+  //     $('.alert').show();
+  //   } else {
+  //     $(".alert").hide();
+  //     $(".new_todo_input").val("");
+  //     $('.btn_submit').attr("data-dismiss", "modal");
+  //   }
+  // });
 
-  $(".btn-danger-edit").click(function(e) {
-    e.preventDefault();
-    $('#myModal_delete').modal()
-    // .one('click', '#delete', function(e){
-    //   $('form').trigger('submit');
-    //  })
-  });
+  // $(".btn-danger-edit").click(function(e) {
+  //   e.preventDefault();
+  //   $('#myModal_delete').modal()
+  //   // .one('click', '#delete', function(e){
+  //   //   $('form').trigger('submit');
+  //   //  })
+  // });
 
-  $(".btn-info-edit").click(function(e) {
-    e.preventDefault();
-    $('#myModal_card_edit').modal();
-  });
+  // $(".btn-info-edit").click(function(e) {
+  //   e.preventDefault();
+  //   $('#myModal_card_edit').modal();
+  // });
 
-  // hide alert upon click events
-  $(".close").click(function() {
-    $(".alert").hide();
-  });
+  // // hide alert upon click events
+  // $(".close").click(function() {
+  //   $(".alert").hide();
+  // });
 
-  $(".btn_add_close").click(function() {
-    $(".alert").hide();
-  });
+  // $(".btn_add_close").click(function() {
+  //   $(".alert").hide();
+  // });
   // End of alert hiding events
 
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,76 +4,176 @@ $(document).ready(function() {
   function createTodoElement(todoObject) {
 
     return card = "<div class=\"card\" id=\"" + todoObject.todo_name + "\">" +
-                    "<img class=\"card-img-top\" src=\"" + todoObject.img_url + "\"/>" +
+                    "<img class=\"card-img-top\" src=\"" + "http://via.placeholder.com/80x60" + "\"/>" +
                     "<div class=\"card-body\">" +
                       "<h4 class=\"card-title\">" + todoObject.todo_name + "</h4>" +
                       "<p class=\"card-text\">Dummie Text</p>" +
                       "<button class=\"btn btn-info btn-info-edit\">Edit</button>" +
                       "<button class=\"btn btn-danger btn-danger-edit\">Delete</button>" +
+                      "<label class=\"is_done_label\"> </label>"
                     "</div>" +
                   "<div>" ;
 
 
   }
 
-  function appendCard() {
+  function appendCard(text) {
     console.log($(".new_todo_input").val());
-    if($(".new_todo_input").val()){
-      console.log("here");
+    if(text){
+      console.log(text);
       $(".movies").append(createTodoElement({}));
     } else {
       console.log("there");
     }
   }
 
-  $(".btn_submit").click(function(e) {
-    e.preventDefault();
-    appendCard();
 
+   $('.alert').hide();
+
+  $(".btn_submit").click(function() {
     //perform ajax post request
-    $.ajax({datatype: "text",
-            url: '/todos',
-            data: $(".new_todo_input").val(),
-            type: 'POST',
-            success: function(responseData, textStatus, jqXHR) {
-              console.log(responseData);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown);
-            }
-    });
-  });
+    console.log($(".new_todo_input").val());
+    var textboxval = $(".new_todo_input").val();
+    appendCard(textboxval);
 
+    // const usercoordinate = navigator.geolocation.getCurrentPosition((position) => {
+    //   const lat  = position.coords.latitude;
+    //   const long = position.coords.longitude;
+    //   $.ajax({datatype: "json",
+    //   url: '/todos',
+    //   data: {text: textboxval,lat: lat , long: long},
+    //   type: 'POST',
+    //   success: function(responseData, textStatus, jqXHR) {
+    //       appendCard(textboxval);
+    //   },
+    //   error: function(jqXHR, textStatus, errorThrown) {
+    //       console.log(errorThrown);
+    //   }
+    // });
 
-  // Delete card
-  $(".btn-danger-edit").on('click', function() {
-    currCard = ($(this).parent().parent());
-    $(".btn_delete_confirmation").click(function(e) {
-      currCard.remove();
-    });
-  });
-
-  $(".btn-info-edit").on('click', function() {
-    currCard = ($(this).parent().parent());
-    $('.btn-card-update').click(function(e) {
-      e.preventDefault();
-      // Updating new name if its not empty
-      if($("rename_todo").val()){
-        currCard.children().children(".card-title").text($("rename_todo").val());
-      }
-
-      // Updating is done, if its not done default to red
-      var btnVal = $('input[name=optradio]:checked').val();
-      if(Number(btnVal) === 1){
-        $(currCard.children().children(".is_done_label")).css("border-bottom", "10px lightgreen solid");
+    $('.btn_submit').removeAttr("data-dismiss");
+      if(!$(".new_todo_input").val()){
+        $('.alert').show();
       } else {
-        $(currCard.children().children(".is_done_label")).css("border-bottom", "10px red solid");
+        $(".alert").hide();
+        $(".new_todo_input").val("");
+        $('.btn_submit').attr("data-dismiss", "modal");
       }
+  });
+
+  var currCardId;
+  var currCard;
+  function editEvents(cate) {
+
+    // danger/delete button clicked
+    $("." + cate).on('click', '.btn-danger-edit', function(e) {
+      e.preventDefault();
+      $('#myModal_delete').modal();
+      var currCardDelete = $(this).closest(".card");
+      $(".btn_delete_confirmation").on('click', function(e) {
+        currCardDelete.remove();
+        //$('#' + cardId).remove();
+      });
     });
-  })
+
+    // info/edit button clicked
+    $("." + cate).on('click', ".btn-info-edit", function(e) {
+      e.preventDefault();
+      $('#myModal_card_edit').modal();
+
+      currCardId = '#' + e.currentTarget.parentElement.parentElement.id;
+      currCard = $(currCardId);
+
+        $('.btn-card-update').click(function(e) {
+        // Updating new name if its not empty
+        if($(".rename_todo").val()){
+          currCard.find('.card-title').text($(".rename_todo").val());
+        }
+
+        // Updating is_done, if its not done default to red
+        var btnVal = $('input[name=optradio]:checked').val();
+        console.log(btnVal);
+        if(Number(btnVal) === 1){
+          $(currCard.find(".is_done_label")).css("border-bottom", "10px lightgreen solid");//toggleclass
+          //currCard.parent().remove(cur);
+          currCard.parent().append(currCard);
+        } else if(Number(btnVal) === 2) {
+          $(currCard.find(".is_done_label")).css("border-bottom", "10px red solid");
+        } else {
+        }
+
+        // Updating category
+        var selectionResult = $('.form-control option:selected').val();
+        switch(selectionResult){
+          case "movie_sele":
+            $("div.movies").append(currCard);
+            break;
+          case "product_sele":
+            $("div.products").append(currCard);
+            break;
+          case "book_sele":
+            $("div.books").append(currCard);
+            break;
+          case "food_sele":
+            $("div.food").append(currCard);
+            break;
+          default:
+            break;
+        }
+
+        // Clear out fields
+        $(".rename_todo").val("");
+        $(".is_done_label").prop('checked', false);
+        $('.form-control').val("");
+      });
+
+    });
+
+      // hide alert upon click events
+    $("." + cate).on('click', ".close", function() {
+      $(".alert").hide();
+    });
+
+    $("." + cate).on('click', ".btn_add_close", function() {
+      $(".alert").hide();
+    });
+
+  }
+
+  editEvents('movies');
+  editEvents('products');
+  editEvents('books');
+  editEvents('food');
+
+
+  // $('.movies').on('click', ".card", function(e) {
+  //   e.preventDefault();
+
+  //   editEvents('movies', $(this).attr("id"));
+  // })
+  // $('.products').on('click', function(e) {
+  //   e.preventDefault();
+
+  //   editEvents($(this).attr("class"), $(this).children().attr("id"));
+  // })
+  // $('.books').on('click', function(e) {
+  //   e.preventDefault();
+
+  //   editEvents($(this).attr("class"), $(this).children().attr("id"));
+  // })
+  // $('.food').on('click', function(e) {
+  //   e.preventDefault();
+
+  //   editEvents($(this).attr("class"), $(this).children().attr("id"));
+  // })
+
+
+
+
 
 
 });
+
 
 
 
