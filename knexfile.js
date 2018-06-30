f886759889a9f6de10a898c19431681ff219f30d
CopterDoctor <Jordananders@me.com>
2018-06-30 14:23:25 -0700

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: './db/migrations'
+      directory: './db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 
