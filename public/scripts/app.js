644aee81714260864edf91779eacc12208a2b721
klynx <KevinLynx_L@hotmail.com>
2018-06-29 17:20:27 -0700

    Added picture scaling and auto categorize to front-end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,15 @@
 // $(document).ready(function() {
 
-
+  // 1 : 'media_todos',
+  // 2 : 'restaurant_todos',
+  // 3 : 'book_todos',
+  // 4 : 'product_todos'
   // This function is for creating cards for each todo
   function createTodoElement(todoObject) {
     switch (todoObject.type_id){
-      case 1:
+      case 1: 
         return card = "<div class=\"card\">" +
-        "<img class=\"card-img-top\" src=\"http://via.placeholder.com/80x60\"/>" +
+        "<div class=\"card-imgbox\"><img class=\"card-img-top\" src=" + todoObject.img +"></div>" +
         "<div class=\"card-body\">" +
           "<h4 class=\"card-title\">Placeholder</h4>" +
           "<p class=\"card-text\">Dummie Text</p>" +
@@ -17,10 +20,10 @@
       
       case 2:
         return card = "<div class=\"card\">" +
-        "<img class=\"card-img-top\" src=\"http://via.placeholder.com/80x60\"/>" +
+        "<div class=\"card-imgbox\"><img class=\"card-img-top\" src=" + todoObject.defaultimage +"></div>" +
         "<div class=\"card-body\">" +
-          "<h4 class=\"card-title\">Placeholder</h4>" +
-          "<p class=\"card-text\">Dummie Text</p>" +
+          "<h4 class=\"card-title\">" + todoObject.name + "</h4>" +
+          "<p class=\"card-text\">" + todoObject.location + "</p>" +
           "<button class=\"btn btn-info btn-info-edit\">Edit</button>" +
           "<button class=\"btn btn-danger btn-danger-edit\">Delete</button>" +
         "</div>" +
@@ -28,9 +31,9 @@
 
       case 3:
         return card = "<div class=\"card\">" +
-        "<img class=\"card-img-top\" src=\"http://via.placeholder.com/80x60\"/>" +
+        "<div class=\"card-imgbox\"><img class=\"card-img-top\" src=" + todoObject.img +"></div>" +
         "<div class=\"card-body\">" +
-          "<h4 class=\"card-title\">Placeholder</h4>" +
+          "<h4 class=\"card-title\">" + todoObject.name + "</h4>" +
           "<p class=\"card-text\">Dummie Text</p>" +
           "<button class=\"btn btn-info btn-info-edit\">Edit</button>" +
           "<button class=\"btn btn-danger btn-danger-edit\">Delete</button>" +
@@ -38,6 +41,17 @@
       "<div>" ;
 
       case 4:
+      return card = "<div class=\"card\">" +
+      "<img class=\"card-img-top\" src=\"http://via.placeholder.com/80x60\"/>" +
+      "<div class=\"card-body\">" +
+        "<h4 class=\"card-title\">Placeholder</h4>" +
+        "<p class=\"card-text\">Dummie Text</p>" +
+        "<button class=\"btn btn-info btn-info-edit\">Edit</button>" +
+        "<button class=\"btn btn-danger btn-danger-edit\">Delete</button>" +
+      "</div>" +
+    "<div>" ;
+
+      default:
         return card = "<div class=\"card\">" +
         "<img class=\"card-img-top\" src=\"http://via.placeholder.com/80x60\"/>" +
         "<div class=\"card-body\">" +
@@ -50,8 +64,18 @@
     }
   }
 
+  // 1 : 'media_todos',
+  // 2 : 'restaurant_todos',
+  // 3 : 'book_todos',
+  // 4 : 'product_todos'
   function appendCard(todoObject) {
-    $(".movies").append(createTodoElement(todoObject));
+    switch (todoObject.type_id){
+      case 1: $(".movies").append(createTodoElement(todoObject)); break;
+      case 2: $(".food").append(createTodoElement(todoObject)); break;
+      case 3: $(".books").append(createTodoElement(todoObject)); break;
+      case 4: $(".products").append(createTodoElement(todoObject)); break;
+    
+    }
   }
 
   // $(".btn_submit").click(function() {
