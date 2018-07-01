3e4157fff0044b0bb763d3d38a91d1ca3d77120e
CopterDoctor <Jordananders@me.com>
2018-07-01 16:57:13 -0700

    set knex.js to env.NODE_ENV for heroku

--- knex.js
+++ knex.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const ENV         = process.env.ENV || "development";
+const ENV         = process.env.NODE_ENV || "development";
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 
