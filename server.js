477e98cf802b149e6ca7972cd93511c0274e50aa
CopterDoctor <Jordananders@me.com>
2018-07-01 17:00:21 -0700

    dotenv interfering with heroku. added conditional check

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const PORT            = process.env.PORT || 8080;
 const express         = require("express");
